c22904eb9d38628d11cda60a0952d4882938c009
mridulaburagohain <93360229+mridulaburagohain@users.noreply.github.com>
2024-11-07 15:07:25 -0500

    Update pick_and_place_moveit.py

--- interbotix_ros_toolboxes/interbotix_common_toolbox/interbotix_moveit_interface/scripts/pick_and_place_moveit.py
+++ interbotix_ros_toolboxes/interbotix_common_toolbox/interbotix_moveit_interface/scripts/pick_and_place_moveit.py
@@ -5,20 +5,7 @@ from threading import Thread
 
 from geometry_msgs.msg import Pose, Quaternion
 from interbotix_moveit_interface_msgs.srv import MoveItPlan
-from python_qt_binding.QtCore import Qt
-from python_qt_binding.QtGui import QDoubleValidator, QFont
-from python_qt_binding.QtWidgets import (
-    QApplication,
-    QHBoxLayout,
-    QLabel,
-    QLineEdit,
-    QPushButton,
-    QSizePolicy,
-    QSlider,
-    QVBoxLayout,
-    QWidget,
-    QComboBox,
-)
+
 import rclpy
 from rclpy.executors import MultiThreadedExecutor
 from rclpy.node import Node
